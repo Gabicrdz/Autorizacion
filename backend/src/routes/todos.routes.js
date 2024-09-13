431060090af5290f8d9809d1451680b9f87cbff5
Gabicrdz <cardozosg2003@gmail.com>
2024-09-13 15:13:04 -0300

    importe las rutas

--- backend/src/routes/todos.routes.js
+++ backend/src/routes/todos.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getAllTodosCtrl } from "../controllers/todos.controllers.js";
+import { getAllTodosCtrl, createTodosCtrl, updateTodosCtrl, deleteTodosCtrl } from "../controllers/todos.controllers.js";
 import validarJwt from "../middlewares/validar-jwt.js";
 
 const todosRouter = Router();
 
 todosRouter.get("/",validarJwt, getAllTodosCtrl);
+todosRouter.post("/",validarJwt, createTodosCtrl);
+todosRouter.put("/:id",validarJwt, updateTodosCtrl);
+todosRouter.delete("/:id",validarJwt, deleteTodosCtrl);
+
 
 export { todosRouter };
