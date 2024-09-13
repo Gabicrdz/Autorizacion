89a9e08164100ed8e76873e5499596f876fd5475
Gabicrdz <cardozosg2003@gmail.com>
2024-09-13 14:59:35 -0300

    constante para cear un nuevo Todos

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -6,3 +6,32 @@ export const getAllTodosCtrl = (req, res) => {
 
   res.json({ todos });
 };
+export const createTodosCtrl = (req, res) => {
+  const { title, completed } = req.body;
+
+  if (!title) {
+    return res.status(400).json({ message: "El título es obligatorio" });
+  } else if (typeof title !== "string") {
+    return res.status(400).json({ message: "El título debe ser un texto" });
+  } else if (title === "") {
+    return res.status(400).json({ message: "El título no puede estar vacío" });
+  }
+
+  if (completed && typeof completed !== "boolean") {
+    return res.status(400).json({ message: "El estado debe ser un booleano" });
+  } else if (completed === "") { 
+    return res.status(400).json({ message: "El estado no puede estar vacío" });
+  }
+  
+  const userId = req.user.id;
+  const newTodo = {
+    id: database.todos.length + 1,
+    title,
+    completed: false,
+    owner: userId,
+  };
+
+  database.todos.push(newTodo);
+
+  res.json({ message: "Tarea creada exitosamente" });
+}
