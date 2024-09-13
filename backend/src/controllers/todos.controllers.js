5f72dc8cfe2acc0118df47d7fd55c142a8bdf349
Gabicrdz <cardozosg2003@gmail.com>
2024-09-13 14:22:03 -0300

    validacion del Jwt

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -1,7 +1,8 @@
 import { database } from "../db/database.js";
 
 export const getAllTodosCtrl = (req, res) => {
-  const todos = database.todos;
+  const userId = req.user.id;
+  const todos = database.todos.filter((todo) => todo.owner === userId);
 
   res.json({ todos });
 };
